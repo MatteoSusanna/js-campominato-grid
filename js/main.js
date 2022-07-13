08a9d2e0a7d9347eeadc9abc6dd52683dd9af3fe
Matteo Susanna <matteo2784@gmail.com>
2022-07-13 15:01:16 +0200

    Al click colori griglia

--- js/main.js
+++ js/main.js
@@ -1,12 +1,50 @@
 const containerDom = document.getElementById('container');
 
+document.getElementById('play').addEventListener('click',
+        function(){
 
-for (let i = 0; i < 100; i++){
+            for (let i = 0; i < 100; i++){
+
+                let element = griglia();
+            
+                element.addEventListener('click',
+                     function(){
+                        this.style.backgroundColor = 'red';
+
+                    }
+                );
+
+            
+            
+            
+               
+                containerDom.append(element);
+            }
+
+        
+        }
+);
 
 
+
+
+
+
+
+
+
+
+
+
+function griglia (){
+    //creazione box compongono la griglia
     let element = document.createElement('div');
     element.classList.add('box');
 
-    containerDom.append(element);
+    //creazione numero che è all'interno del box
+    let numero = document.createElement('div');
+    numero.classList.add('numeroInterno');
+    element.append(numero);
 
+    return element;
 }
