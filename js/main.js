abc30f717036aa3d0ccbd9a2c1aa797ef203054b
Matteo Susanna <matteo2784@gmail.com>
2022-07-13 16:03:26 +0200

    Inserimento numero progressivi

--- js/main.js
+++ js/main.js
@@ -1,22 +1,22 @@
 const containerDom = document.getElementById('container');
 
+//evento click crea gliglia
 document.getElementById('play').addEventListener('click',
         function(){
 
-            for (let i = 0; i < 100; i++){
+            //ciclo per creare 100 box nella griglia
+            for (let i = 1; i <= 100; i++){
 
-                let element = griglia();
-            
+
+                let element = griglia(i);
+                
+                //evento click cambio colore red
                 element.addEventListener('click',
                      function(){
                         this.style.backgroundColor = 'red';
 
                     }
                 );
-
-            
-            
-            
                
                 containerDom.append(element);
             }
@@ -26,25 +26,18 @@ document.getElementById('play').addEventListener('click',
 );
 
 
+//funzione per creare la griglia
 
-
-
-
-
-
-
-
-
-
-function griglia (){
+function griglia (numeroInternoAlBox){
     //creazione box compongono la griglia
     let element = document.createElement('div');
     element.classList.add('box');
 
     //creazione numero che è all'interno del box
-    let numero = document.createElement('div');
-    numero.classList.add('numeroInterno');
-    element.append(numero);
+    let numeroInterno = document.createElement('div');
+    numeroInterno.classList.add('numeroInterno');
+    numeroInterno.append(numeroInternoAlBox);
 
+    element.append(numeroInterno);
     return element;
 }
